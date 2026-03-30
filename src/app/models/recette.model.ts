7f8ce211808004bd2e71e5046d0bb6668a95fbc6
EnzoPss <enzopousse.06@gmail.com>
2026-03-30 12:04:16 +0200

    patch

--- src/app/models/recette.model.ts
+++ src/app/models/recette.model.ts
@@ -25,8 +25,8 @@ description: string;
 surgraissage: number;
 apportEnEau: number;
 avecSoude: boolean;
-concentrationAlcali: number;
-qteAlcali: number;
+concentrationAlcalin: number; // Rajouter un 'n' à la fin
+qteAlcalin: number; // Rajouter un 'n' à la fin
 ligneIngredients: LigneIngredient[];
 resultats: Resultat[];
 }
--- src/app/pages/recipe-manager-page/recipe-manager-page.ts
+++ src/app/pages/recipe-manager-page/recipe-manager-page.ts
@@ -1,11 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recette } from '../../models/recette.model';
+import { RecetteService } from '../../services/recette.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-recipe-manager-page',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './recipe-manager-page.html',
   styleUrl: './recipe-manager-page.css',
 })
-export class RecipeManagerPage {
 
+export class RecipeManagerPage implements OnInit {
+  public recettes: Recette[] = []
+  constructor(private recetteService: RecetteService) { }
+
+  // Dans la classe RecipeManagerPage
+  // Propriété pour stocker la recette à afficher dans la modale
+  public recetteSelectionnee: Recette | null = null;
+  /**
+  * Définit la recette sélectionnée pour l'affichage des détails
+  */
+  ouvrirModale(recette: Recette): void {
+    this.recetteSelectionnee = recette;
+  }
+  /**
+  * Réinitialise la sélection à la fermeture
+  */
+  fermerModale(): void {
+    this.recetteSelectionnee = null;
+  }
+
+  ngOnInit(): void {
+    this.chargerRecettes();
+  }
+
+  chargerRecettes(): void {
+    this.recetteService.getRecettes().subscribe({
+      next: (data) => this.recettes = data,
+      error: (err) => console.error("Erreur API", err)
+    })
+  }
+
+  supprimerRecette(id: number): void {
+    if (confirm("Supprimer cette recette ?")) {
+      this.recetteService.deleteRecette(id).subscribe(() =>
+        this.chargerRecettes());
+    }
+  }
 }
